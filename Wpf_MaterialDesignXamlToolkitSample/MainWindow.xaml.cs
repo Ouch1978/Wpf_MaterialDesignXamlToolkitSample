8ace01d87ca824da4a5c1c05d2e750b6d9f1db7a
Ouch1978 <ouch1978@hotmail.com>
2017-03-25 02:41:22 +0800

    First commit

---
+++ Wpf_MaterialDesignXamlToolkitSample/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using MahApps.Metro.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Wpf_MaterialDesignXamlToolkitSample
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : MetroWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSendMessage_Click(object sender, RoutedEventArgs e)
+        {
+            var messageQueue = snackBar1.MessageQueue;
+            var message = txtMessage.Text;
+            Task.Factory.StartNew(() => messageQueue.Enqueue(message));
+        }
+    }
+}
